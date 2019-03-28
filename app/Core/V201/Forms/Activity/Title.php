2ad53288a4f4d42c67a123a965001b44b7aeca21
Rumy <a-saleque@dfid.gov.uk>
2019-03-28 13:08:18 +0600

    Converted the dropdown Language to a hidden field and set the language to english by default for Narratives. Removed the add more option from the title field.

--- app/Core/V201/Forms/Activity/Title.php
+++ app/Core/V201/Forms/Activity/Title.php
@@ -17,7 +17,7 @@ class Title extends BaseForm
         $class = $this->getData('class') ? $this->getData('class') : 'narrative';
         $bool  = ($this->getData('narrative_true') || !$this->getName()) ? true : false;
         $this
-            ->addNarrative($class, trans('elementForm.text'), ['narrative_required' => $bool])
-            ->addAddMoreButton('add_title', $class);
+            ->addNarrative($class, trans('elementForm.text'), ['narrative_required' => $bool]);
+            //->addAddMoreButton('add_title', $class);
     }
 }
