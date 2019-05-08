f865dd1c9051e129d9462d9a856d95a4b7e844b8
Rumy <a-saleque@dfid.gov.uk>
2019-05-08 16:36:52 +0600

    Added a budget definition.

--- resources/lang/en/elementForm.php
+++ resources/lang/en/elementForm.php
@@ -280,5 +280,6 @@ return [
     'indicator_or_code'                          => 'Indicator URI or Code',
     'value_if_nonqualitative'                    => 'Numeric Value must be included for Non-qualitative Indicator Measure',
     'value_if_qualitative'                       => 'Value must be empty for Qualitative Indicator Measure',
+    'budget'                                     => 'Budgets'
 ];
 
