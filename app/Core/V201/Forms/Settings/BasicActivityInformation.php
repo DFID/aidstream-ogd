5c36bf751391c0abab2c41256f5703782023c3bc
Rumy <a-saleque@dfid.gov.uk>
2019-05-12 14:59:23 +0600

    Fixed a small bug on the settings page.

--- app/Core/V201/Forms/Settings/BasicActivityInformation.php
+++ app/Core/V201/Forms/Settings/BasicActivityInformation.php
@@ -14,10 +14,10 @@ class BasicActivityInformation extends BaseForm
     public function buildForm()
     {
         $this
-            ->addCheckBox('title', trans('element.title'), true, '')
-            ->addCheckBox('description', trans('element.description'), true, '')
-            ->addCheckBox('activity_status', trans('element.activity_status'), true, '')
-            ->addCheckBox('activity_date', trans('element.activity_date'), true, '')
+            ->addCheckBox('title', trans('element.title'))
+            ->addCheckBox('description', trans('element.description'))
+            ->addCheckBox('activity_status', trans('element.activity_status'))
+            ->addCheckBox('activity_date', trans('element.activity_date'))
             ->addCheckBox('contact_info', trans('element.contact_info'))
             ->addCheckBox('activity_scope', trans('element.activity_scope'))
             ->addCheckBox('basic_activity_info', trans('element.basic_activity_info_(lite)'));
