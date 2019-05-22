85d368048f65e524b6c8c79a10afc235e2bad9d5
Rumy <a-saleque@dfid.gov.uk>
2019-05-22 13:20:20 +0600

    Default policy marker values are auto selected as 0.

--- app/Core/V203/Forms/Activity/PolicyMarker.php
+++ app/Core/V203/Forms/Activity/PolicyMarker.php
@@ -12,15 +12,15 @@ class PolicyMarker extends BaseForm
     public function buildForm()
     {
         $this
-            ->addSelect('significance_1', $this->getCodeList('PolicySignificance', 'Activity'), trans('elementForm.significance_1'))
-            ->addSelect('significance_2', $this->getCodeList('PolicySignificance', 'Activity'), trans('elementForm.significance_2'))
-            ->addSelect('significance_3', $this->getCodeList('PolicySignificance', 'Activity'), trans('elementForm.significance_3'))
-            ->addSelect('significance_4', $this->getCodeList('PolicySignificance', 'Activity'), trans('elementForm.significance_4'))
-            ->addSelect('significance_5', $this->getCodeList('PolicySignificance', 'Activity'), trans('elementForm.significance_5'))
-            ->addSelect('significance_6', $this->getCodeList('PolicySignificance', 'Activity'), trans('elementForm.significance_6'))
-            ->addSelect('significance_7', $this->getCodeList('PolicySignificance', 'Activity'), trans('elementForm.significance_7'))
-            ->addSelect('significance_8', $this->getCodeList('PolicySignificance', 'Activity'), trans('elementForm.significance_8'))
-            ->addSelect('significance_9', $this->getCodeList('PolicySignificance', 'Activity'), trans('elementForm.significance_9'))
+            ->addSelect('significance_1', $this->getCodeList('PolicySignificance', 'Activity'), trans('elementForm.significance_1'),null,0)
+            ->addSelect('significance_2', $this->getCodeList('PolicySignificance', 'Activity'), trans('elementForm.significance_2'),null,0)
+            ->addSelect('significance_3', $this->getCodeList('PolicySignificance', 'Activity'), trans('elementForm.significance_3'),null,0)
+            ->addSelect('significance_4', $this->getCodeList('PolicySignificance', 'Activity'), trans('elementForm.significance_4'),null,0)
+            ->addSelect('significance_5', $this->getCodeList('PolicySignificance', 'Activity'), trans('elementForm.significance_5'),null,0)
+            ->addSelect('significance_6', $this->getCodeList('PolicySignificance', 'Activity'), trans('elementForm.significance_6'),null,0)
+            ->addSelect('significance_7', $this->getCodeList('PolicySignificance', 'Activity'), trans('elementForm.significance_7'),null,0)
+            ->addSelect('significance_8', $this->getCodeList('PolicySignificance', 'Activity'), trans('elementForm.significance_8'),null,0)
+            ->addSelect('significance_9', $this->getCodeList('PolicySignificance', 'Activity'), trans('elementForm.significance_9'),null,0)
             ;
     }
 }
