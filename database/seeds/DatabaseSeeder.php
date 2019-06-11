51252f3bbb80c6acc6e93062f58f543b08a864da
Rumy <a-saleque@dfid.gov.uk>
2019-06-11 13:45:35 +0600

    Added the permission levels for a user in aidstream lite.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder {
 		$this->call('UserTableSeeder');
 		$this->call('VersionTableSeeder');
 		$this->call('RoleTableSeeder');
+		$this->call('RoleTableUpdateSeeder');
 	}
 
 }
