478695c8bcd05553981df9afffb55874eb94a676
Rumy <a-saleque@dfid.gov.uk>
2019-09-12 09:55:09 +0600

    Fixed a small bug

--- app/Core/V203/Forms/Organization/DocumentLinkForm.php
+++ app/Core/V203/Forms/Organization/DocumentLinkForm.php
@@ -25,8 +25,8 @@ class DocumentLinkForm extends BaseForm
                 ]
             )
             ->addSelect('format', $this->getCodeList('FileFormat', 'Organization'), trans('elementForm.format'), null, null, true)
-            ->addNarrative('narrative')
-            ->addAddMoreButton('add_narrative', 'narrative')
+            ->addNarrative('narrative hide-two')
+            //->addAddMoreButton('add_narrative', 'narrative')
             ->addCollection('description', 'Activity\Title', '', [], trans('elementForm.description'))
             ->addCollection('category', 'Organization\CategoryCodeForm', 'category', [], trans('elementForm.category'))
             ->addAddMoreButton('add_category', 'category')
@@ -34,7 +34,7 @@ class DocumentLinkForm extends BaseForm
             ->addAddMoreButton('add_language', 'language')
             ->addCollection('document_date', 'Organization\PeriodStart', '', [], trans('elementForm.document_date'))
             ->addCollection('recipient_country', 'Organization\RecipientCountryForm', 'recipient_country', [], trans('elementForm.recipient_country'))
-            ->addAddMoreButton('add_recipient_country', 'recipient_country')
+            //->addAddMoreButton('add_recipient_country', 'recipient_country')
             ->addRemoveThisButton('remove_document_link');
     }
 }
