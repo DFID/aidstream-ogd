1696eb4b3b5517bbfa295843f6fbee9c96e3f409
aunindadf <a-saleque@dfid.gov.uk>
2022-09-15 15:19:47 +0600

    Update uploader.blade.php

--- resources/views/Activity/transaction/uploader.blade.php
+++ resources/views/Activity/transaction/uploader.blade.php
@@ -38,11 +38,11 @@
                                 <div>@lang('global.simple_transaction_text')
                                 </div>
                             </div>
-                            <div class="download-transaction-wrap">
+                            <!-- <div class="download-transaction-wrap">
                                 <a href="{{route('activity.import-transaction.download-detailed-csv').'?'.date('Y-m-d H:i:s')}}" class="btn btn-primary btn-form btn-submit">@lang('global.download_detailed_transaction_template')</a>
                                 <div>@lang('global.detailed_transaction_text')
                                 </div>
-                            </div>
+                            </div> -->
                         </div>
                     </div>
                 </div>
